5d3fb3fa2969cf90bc3363b79b0fe8bbd545b873
Luana Domeneghetti <luanaa.domneghettii@gmail.com>
2024-10-03 17:03:52 -0300

    create post method

--- src/routes/planetas.routes.js
+++ src/routes/planetas.routes.js
@@ -3,96 +3,109 @@ import { Router } from "express";
 const planetasRoutes = Router();
 
 let planetas = [
-{
+  {
     id: Number(Math.floor(Math.random() * 999999) + 1),
     nome: "Dev",
     temperatura: "13.3",
     agua: false, // Indicação de existência de água
-    atm: [ "JS", "NODE", "VS", "Code",]
-}
-];
-
+    atm: ["JS", "NODE", "VS", "Code"],
+  },
+]
 
 //Rotas para buscar todos os  elementos do array planetas.
 planetasRoutes.get("/", (req, res) => {
   return res.status(200).send(planetas);
 });
 
-// Rota para criar novo filme marcante.
+// Rota para cadastrar um novo planeta.
 planetasRoutes.post("/", (req, res) => {
-  const { titulo, genero, emCartaz } = req.body;
+  const { nome, temperatura, agua, atm } = req.body;
 
-  const novoFilme = {
-    id: Number(Math.floor(Math.random() * 99) + 1),
-    titulo,
-    genero,
-    emCartaz,
+  if (!nome || !temperatura || !agua) {
+    return res.status(400).send({
+      message: "Os campos nome, temperatura e água são obrigatórios!",
+    });
+  }
+
+  // Validação de existência de água
+  if (agua != "Sim" && agua != "Não") {
+    return res.status(400).send({
+            message: "Digite 'sim' ou 'não'!",
+        });
+  }
+
+  const novoPlaneta = {
+    id: Number(Math.floor(Math.random() * 999999) + 1),
+    nome,
+    temperatura,
+    agua,
+    atm,
   };
 
-  planetas.push(novoFilme);
+  planetas.push(novoPlaneta);
 
-  return res.status(201).send(planetas);
+  return res.status(201).send({
+    message: "Planeta cadastrado!",
+    novoPlaneta,
+  });
 });
 
-
 // Rota para buscar um elemento específico do array planetas
-planetasRoutes.get("/:id", (req, res) => { 
-    const { id } = req.params
+planetasRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-    console.log(id);
+  console.log(id);
 
-    const filme = planetas.find((movie) => movie.id === Number(id));
+  const filme = planetas.find((movie) => movie.id === Number(id));
 
-    //console.log(filme);
+  //console.log(filme);
 
-    if (!filme) {
-        return res.status(404).send({ message: "Filme não encontrado!"});
-    }
+  if (!filme) {
+    return res.status(404).send({ message: "Filme não encontrado!" });
+  }
 
-    return res.status(200).send(filme);
+  return res.status(200).send(filme);
 });
 
 // Rota para editar um filme
-planetasRoutes.put("/:id", (req, res) => { 
-    const { id } = req.params
+planetasRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
 
-    const filme = planetas.find((movie) => movie.id === Number(id));
+  const filme = planetas.find((movie) => movie.id === Number(id));
 
-    //console.lo filme);
+  //console.lo filme);
 
-    if (!filme) {
-        return res.status(404).send({ message: "Filme não encontrado!"});
-    }
+  if (!filme) {
+    return res.status(404).send({ message: "Filme não encontrado!" });
+  }
 
-    const { titulo, genero, emCartaz } = req.body;
+  const { titulo, genero, emCartaz } = req.body;
 
- filme.titulo = titulo
- filme.genero = genero
- filme.emCartaz = emCartaz
-    
-    return res.status(200).send({
-        message: "Filme atualizado!",
-        filme,
-    }
-)
+  filme.titulo = titulo;
+  filme.genero = genero;
+  filme.emCartaz = emCartaz;
+
+  return res.status(200).send({
+    message: "Filme atualizado!",
+    filme,
+  });
 });
 
 // Rota para deletar um filme marcante.
-planetasRoutes.delete("/:id", (req, res) => { 
-    const { id } = req.params
+planetasRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
 
-    const filme = planetas.find((movie) => movie.id === Number(id));
+  const filme = planetas.find((movie) => movie.id === Number(id));
 
+  if (!filme) {
+    return res.status(404).send({ message: "Filme não encontrado!" });
+  }
 
-    if (!filme) {
-        return res.status(404).send({ message: "Filme não encontrado!"});
-    }
-    
-    planetas = planetas.filter((movie) => movie.id !== Number(id));
-    return res.status(200).send({
-        message: "filme deletado!",
-        filme
-    })
+  planetas = planetas.filter((movie) => movie.id !== Number(id));
+  return res.status(200).send({
+    message: "filme deletado!",
+    filme,
+  });
 });
 
 export default planetasRoutes;
